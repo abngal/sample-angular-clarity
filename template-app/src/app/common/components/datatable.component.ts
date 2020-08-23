44d36c55d1c6faebf5befaac5bd807530b8b57b9
ab <x>
2020-08-24 03:27:05 +0800

    adds datatable sample

--- template-app/src/app/common/components/datatable.component.ts
+++ template-app/src/app/common/components/datatable.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+// import { mockUserList } from 'src/app/data/mock';
+import { mockUserList } from '@data/mock';
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.scss']
 })
 export class DatatableComponent implements OnInit {
+
+  @Input() users = mockUserList;
 
   constructor() { }
 
